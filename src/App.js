1111db41e978ade8c0a070ff21e33da45a6345c8
jeybone <jeysonp@gmail.com>
2019-03-11 15:21:53 -0500

    added components, and header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Game from './components/Game';
+import BandPics from './components/BandPics';
+
 
 class App extends Component {
-  render() {
+  state = {
+    score: 0,
+    highScore: 0,
+  };
+
+render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Game />
+        <BandPics />
+      
       </div>
     );
   }
---
+++ src/components/BandPics.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class BandPics extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default BandPics
